174fcd571eacb457f997be1157baba21a223de05
rayconghou <rayconghou@gmail.com>
2025-03-14 02:04:55 -0500

    3.14 #3

--- twosum2.py
+++ twosum2.py
@@ -18,3 +18,26 @@ class Solution(object):
                 end -= 1
             else:
                 start += 1
+
+# raymond 
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        start = 0
+        end = len(numbers)-1
+
+        while(start < end):
+            total = numbers[start] + numbers[end]
+            if total > target:
+                end -= 1
+            elif total == target:
+                return [start+1, end+1]
+            else:
+                start += 1
+    
+        # answer should be guaranteed
+        return "something went wrong"
