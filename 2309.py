8d762944bd7e0a05f6471480a32076bc0a438aa7
jordanylee <jordanylee2023@gmail.com>
2025-03-14 16:18:05 -0500

    2309

---
+++ 2309.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def greatestLetter(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ascii_val_A = ord('A')
+        ascii_val_a = ord('a')
+
+        lowerChars = [0] * 26
+        upperChars = [0] * 26
+        greatest_letter = ""
+        for char in s:
+            if char.isupper():
+                upperChars[ord(char) - ascii_val_A] += 1
+
+                if lowerChars[ord(lower(char)) - ascii_val_a] == 0:
+                    continue
+                if char > greatest_letter:
+                    greatest_letter = char
+            else:
+                lowerChars[ord(char) - ascii_val_a] += 1
+
+                if upperChars[ord(upper(char)) - ascii_val_A] == 0:
+                    continue
+                if upper(char) > greatest_letter:
+                    greatest_letter = upper(char)
+
+        return greatest_letter
