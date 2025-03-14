a666e42db62593a66b37029f8640af5ad1ac03c9
jordanylee <jordanylee2023@gmail.com>
2025-03-14 01:57:58 -0500

    twosum2

---
+++ twosum2.py
@@ -0,0 +1,20 @@
+#jordan
+
+class Solution(object): 
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        start = 0
+        end = len(numbers) - 1
+
+        while True:
+            sum = numbers[start] + numbers[end]
+            if sum == target:
+                return [start + 1, end + 1]
+            if sum > target:
+                end -= 1
+            else:
+                start += 1
