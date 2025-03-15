c646c0aabd3b013033a40996f39855bd59a69126
jordanylee <jordanylee2023@gmail.com>
2025-03-15 01:28:39 -0500

    binarysearch

--- binarysearch.py
+++ binarysearch.py
@@ -1,3 +1,25 @@
+#jrodan 100 63.81
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        return self.binarySearch(nums, target, 0, (len(nums) - 1))
+
+    def binarySearch(self, nums, target, lower, upper):
+        if lower > upper:
+            return -1
+        midpoint_index = ((upper + lower) // 2)
+        midpointNum = nums[midpoint_index]
+        if midpointNum == target:
+            return midpoint_index
+        if midpointNum > target:
+            return self.binarySearch(nums, target, lower, (midpoint_index - 1))
+        else:
+            return self.binarySearch(nums, target, (midpoint_index + 1), upper)
+
 # raymond t100 m63
 class Solution(object):
     def search(self, nums, target):
