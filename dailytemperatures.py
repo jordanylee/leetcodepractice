8dc654559ba74119c1e273df240d4dfc4389ffeb
rayconghou <rayconghou@gmail.com>
2025-03-15 00:54:01 -0500

    r-dailytemp

--- dailytemperatures.py
+++ dailytemperatures.py
@@ -18,3 +18,22 @@ class Solution(object):
             stack.append((index, temperature))
         
         return days_to_wait
+    
+# raymond t43 m23
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        res = [0] * len(temperatures)
+        stack = []
+
+        for i in range(len(temperatures)):
+            while stack and stack[-1][1] < temperatures[i]:
+                index = stack[-1][0]
+                res[index] = i - index
+                del stack[-1]
+            stack.append((i, temperatures[i]))
+
+        return res
