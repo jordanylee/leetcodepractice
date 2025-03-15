b914d405ca2f135ceaa0c7fde7378031fb2acb43
rayconghou <rayconghou@gmail.com>
2025-03-15 00:29:13 -0500

    r-minstack

--- minstack.py
+++ minstack.py
@@ -46,3 +46,48 @@ class MinStack(object):
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+# raymond t90 m18
+
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        if self.stack:
+            self.stack.append((val, min(val, self.stack[-1][1])))
+        else:
+            self.stack.append((val, val))
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
